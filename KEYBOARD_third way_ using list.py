e88940f78970bb415dcbb75e7101e984ddb9e20d
crazyboomer <78368938+varun2001-vit@users.noreply.github.com>
2021-05-24 19:45:00 +0530

    Update KEYBOARD_third way_ using list.py

--- KEYBOARD_third way_ using list.py
+++ KEYBOARD_third way_ using list.py
@@ -33,9 +33,7 @@ def arm_and_takeoff(aTargetAltitude):
     print("Taking off!")
     vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
 
-    # Wait until the vehicle reaches a safe height before processing the goto
-    #  (otherwise the command after Vehicle.simple_takeoff will execute
-    #   immediately).
+   
     while True:
         print(" Altitude: ", vehicle.location.global_relative_frame.alt)
         # Break and return from function just below target altitude.
@@ -44,19 +42,19 @@ def arm_and_takeoff(aTargetAltitude):
             break
         time.sleep(1)
     
-def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
+def send_ned_velocity(velx, vely, velz, duration):
     """
     Move vehicle in direction based on specified velocity vectors.
     """
     msg = vehicle.message_factory.set_position_target_local_ned_encode(
-        0,       # time_boot_ms (not used)
-        0, 0,    # target system, target component
+        0,       
+        0, 0,    
         mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
-        0b0000111111000111, # type_mask (only speeds enabled)
+        0b0000111111000111, 
         0, 0, 0, # x, y, z positions (not used)
-        velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
-        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
-        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+        vel_x, vel_y, vel_z, # x, y, z velocity in m/s
+        0, 0, 0, 
+        0, 0)    
 
 
     # send command to vehicle 
@@ -74,11 +72,11 @@ def condition_yaw(heading, relative=False):
         0, 0,    # target system, target component
         mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
         0, #confirmation
-        heading,    # param 1, yaw in degrees
-        0,          # param 2, yaw speed deg/s
-        1,          # param 3, direction -1 ccw, 1 cw
-        is_relative, # param 4, relative offset 1, absolute angle 0
-        0, 0, 0)    # param 5 ~ 7 not used
+        heading,    
+        0,          
+        1,          
+        is_relative, 
+        0, 0, 0)    
     # send command to vehicle
     vehicle.send_mavlink(msg)
 
